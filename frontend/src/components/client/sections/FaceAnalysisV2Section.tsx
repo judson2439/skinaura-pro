a25546aaa2755b5fcadc34959fa6f2f790835068
superstar643 <fullstackdev2439@gmail.com>
2026-01-12 13:18:10 -0500

    updates

--- frontend/src/components/client/sections/FaceAnalysisV2Section.tsx
+++ frontend/src/components/client/sections/FaceAnalysisV2Section.tsx
@@ -15,7 +15,7 @@ const FaceAnalysisV2Section: React.FC = () => {
   const options = {
     faceageId: FACE_AGE_ID,
     displayModel: 'widget',
-    type: 'skincare-analyzer',
+    type: 'skincare',
     language: 'en',
     currency: '$',
     quiz: true,
