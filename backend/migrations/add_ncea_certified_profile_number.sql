2d745f894ed90e9fec79642637f6a7c91a55029a
judson2439 <judson.gerhold@outlook.com>
2026-02-11 18:37:00 +0000

    updates

--- backend/migrations/add_ncea_certified_profile_number.sql
+++
@@ -1,7 +0,0 @@
--- Add NCEA Certified Profile Number for professionals
--- Run this migration against your database before deploying the backend change.
-
-ALTER TABLE user_profiles
-  ADD COLUMN IF NOT EXISTS ncea_certified_profile_number TEXT;
-
-COMMENT ON COLUMN user_profiles.ncea_certified_profile_number IS 'NCEA certified profile number for registered professionals';
